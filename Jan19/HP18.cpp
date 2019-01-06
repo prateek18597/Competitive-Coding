8fa767cc2f4f5be3eb9c50db67f93d21f740b090
Pratik Parmar <prateek18597@gmail.com>
2019-01-06 11:22:58 +0000

    HP18

---
+++ Jan19/HP18.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int T;
+	cin>>T;
+	for(int i=0;i<T;i++)
+	{
+		long long int N,a,b;
+		long long int countBob=0,countAlice=0,countBoth=0;
+		cin>>N>>a>>b;
+		long long int A[N];
+		for(long long int j=0;j<N;j++)
+		{
+			cin>>A[j];
+			if(A[j]%a==0)
+			{
+				countBob++;
+			}
+			if(A[j]%b==0)
+			{
+				countAlice++;
+			}
+			if(A[j]%b==0 && A[j]%a==0)
+			{
+				countBoth++;
+				countBob--;
+				countAlice--;
+			}
+		}
+		if(b%a==0)
+		{
+			cout<<"BOB"<<endl;
+		}
+		else
+		{
+			if(countBob>countAlice)
+			{
+				cout<<"BOB"<<endl;
+			}
+			else
+			{
+				if()
+			}
+		}
+	}
+}
